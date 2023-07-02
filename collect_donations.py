4df5b91a8b5a6808e74715c908450814f1d70cff
Ruslan Kovtun <koutoftimer@gmail.com>
2023-07-02 11:05:23 +0300

    Add migration for users table

--- collect_donations.py
+++ collect_donations.py
@@ -2,12 +2,13 @@
 import json
 import logging
 import os
-import sqlite3
 import subprocess
 from pathlib import Path
-from typing import Iterable
 
 import requests
+import pyautogui
+
+from .db import save_donations_to_db
 
 DONATIONS_RECT = 448, 174, 535, 885
 
@@ -17,7 +18,6 @@ class MainScreen:
 
     @staticmethod
     def go_to_guild_screen():
-        import pyautogui
         pyautogui.moveTo(*MainScreen.GUILD_BUTTON_LOC)
         pyautogui.click()
         pyautogui.sleep(1)
@@ -28,7 +28,6 @@ class GuildScreen:
 
     @staticmethod
     def open_treasury():
-        import pyautogui
         pyautogui.moveTo(*GuildScreen.TREASURE_BUTTON_LOC)
         pyautogui.click()
         pyautogui.sleep(1)
@@ -38,7 +37,6 @@ DONATION_SCREENSHOTS = [Path('1.png'), Path('2.png')]
 
 
 def take_donations_screenshots():
-    import pyautogui
     for path in DONATION_SCREENSHOTS:
         if path.exists():
             path.unlink()
@@ -71,6 +69,8 @@ def ocr_screenshot(screenshot: str) -> dict[str, int]:
     ''')
     NICK_CORRECTIONS = {
         'eItooth': 'eltooth',
+        'シ ヤ ガー': 'ジャガー',
+        'シ ヤ ガ 一': 'ジャガー',
     }
     data = {}
     for line in raw.splitlines():
@@ -89,66 +89,6 @@ def ocr(screenshots: list[Path]):
     return data
 
 
-DB_FILENAME = './guild.db'
-
-
-def save_donations_to_db(data: dict[str, int]):
-    logging.info(f'Updating donations: {data}')
-    update_member_status(data.keys())
-    create_table = '''
-    CREATE TABLE IF NOT EXISTS donations (
-        user_id INTEGER NOT NULL,
-        donation INTEGER NOT NULL,
-        timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        id INTEGER PRIMARY KEY ASC,
-        FOREIGN KEY(user_id) REFERENCES user(id)
-    )
-    '''
-    conn = sqlite3.connect(DB_FILENAME)
-    cur = conn.cursor()
-    cur.execute(create_table)
-    cur.executemany(
-        '''
-        INSERT INTO donations (user_id, donation)
-        VALUES ((SELECT id FROM users WHERE nickname = ?), ?)
-        ''',
-        tuple(data.items()),
-    )
-    conn.commit()
-
-
-def update_member_status(active_members: Iterable[str]):
-    create_table = '''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY ASC,
-        nickname TEXT UNIQUE NOT NULL,
-        status TEXT NOT NULL,
-        comment TEXT
-    )
-    '''
-    conn = sqlite3.connect(DB_FILENAME)
-    cur = conn.cursor()
-    cur.execute(create_table)
-
-    # mark active member as leavers
-    user_list = ','.join(f'"{v}"' for v in active_members)
-    cur.execute(
-        'UPDATE users SET status = "left" '
-        f'WHERE users.status = "active" AND users.nickname NOT IN ({user_list})'
-    )
-    # add new members
-    cur.executemany(
-        'INSERT OR IGNORE INTO users (nickname, status) VALUES (?, "active")',
-        zip(active_members),
-    )
-    # activate existing members
-    cur.executemany(
-        'UPDATE users SET status = "active" WHERE users.nickname = ?',
-        zip(active_members),
-    )
-    conn.commit()
-
-
 def push_updates_to_pythonanywhere(data: dict[str, int]):
     sync_host = os.environ.get('SYNC_HOST', 'outoftime.pythonanywhere.com')
     logging.info(f"Sync DB with {sync_host}: {data}")
@@ -162,14 +102,14 @@ def push_updates_to_pythonanywhere(data: dict[str, int]):
 
 
 def main():
-    import pyautogui
-    logging.getLogger().setLevel(logging.INFO)
+    logging.getLogger().setLevel(logging.DEBUG)
     pyautogui.FAILSAFE = True
     # TODO: rewrite with python ffi for libx11
     subprocess.getoutput(
         "xdotool search --name '^Firestone$' windowactivate --sync")
     take_donations_screenshots()
     data = ocr(DONATION_SCREENSHOTS)
+    logging.debug(f'OCR {data}')
     save_donations_to_db(data)
     # replicate data to pythonanywhere
     push_updates_to_pythonanywhere(data)
---
+++ db.py
@@ -0,0 +1,136 @@
+import logging
+import sqlite3
+from typing import Iterable
+
+DB_FILENAME = './guild.db'
+
+
+def save_donations_to_db(data: dict[str, int]):
+    logging.info(f'Updating donations: {data}')
+    update_member_status(data.keys())
+    create_table = '''
+    CREATE TABLE IF NOT EXISTS donations (
+        user_id INTEGER NOT NULL,
+        donation INTEGER NOT NULL,
+        timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        id INTEGER PRIMARY KEY ASC,
+        FOREIGN KEY(user_id) REFERENCES user(id)
+    )
+    '''
+    conn = sqlite3.connect(DB_FILENAME)
+    cur = conn.cursor()
+    cur.execute(create_table)
+    cur.executemany(
+        '''
+        INSERT INTO donations (user_id, donation)
+        VALUES ((SELECT id FROM users WHERE nickname = ?), ?)
+        ''',
+        tuple(data.items()),
+    )
+    conn.commit()
+
+
+def update_member_status(active_members: Iterable[str]):
+    create_table = '''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY ASC,
+        nickname TEXT UNIQUE NOT NULL,
+        status TEXT NOT NULL,
+        comment TEXT
+    )
+    '''
+    conn = sqlite3.connect(DB_FILENAME)
+    cur = conn.cursor()
+    cur.execute(create_table)
+
+    # mark active member as leavers
+    user_list = ','.join(f'"{v}"' for v in active_members)
+    cur.execute(
+        'UPDATE users SET status = "left" '
+        f'WHERE users.status = "active" AND users.nickname NOT IN ({user_list})'
+    )
+    # add new members
+    cur.executemany(
+        'INSERT OR IGNORE INTO users (nickname, status) VALUES (?, "active")',
+        zip(active_members),
+    )
+    # activate existing members
+    cur.executemany(
+        'UPDATE users SET status = "active" WHERE users.nickname = ?',
+        zip(active_members),
+    )
+    conn.commit()
+
+
+# Migrations
+
+
+def alter_users_table():
+    migration = '''
+    -- Add changed_at and created_at columns to users table
+    BEGIN;
+
+    -- Create temporary table
+    CREATE TABLE IF NOT EXISTS users_new (
+        id INTEGER PRIMARY KEY ASC,
+        nickname TEXT UNIQUE NOT NULL,
+        status TEXT NOT NULL,
+        changed_at NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        created_at NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        comment TEXT
+    );
+
+    -- Data migration
+    INSERT INTO users_new (id, nickname, status, comment)
+        SELECT * FROM users;
+
+    -- Replace temporary table with original
+    DROP TABLE users;
+    ALTER TABLE users_new RENAME TO users;
+
+    -- Set correct creation time (this is correct only now)
+    UPDATE users
+       SET created_at = (SELECT min(timestamp) FROM donations WHERE user_id = users.id),
+           changed_at = ifnull(
+               (SELECT min(timestamp)
+                 FROM donations
+                WHERE timestamp > (
+                    SELECT max(timestamp)
+                      FROM (
+                         SELECT timestamp, group_concat('<' || user_id || '>') as members
+                           FROM donations
+                         GROUP BY timestamp
+                      )
+                     WHERE members NOT LIKE '%<' || users.id || '>%'
+                )),
+                (SELECT min(timestamp) FROM donations WHERE user_id = users.id)
+           );
+
+    -- changed_at differes for those who left
+    UPDATE users
+       SET changed_at = (
+           SELECT max(timestamp) FROM donations WHERE user_id = users.id
+       )
+     WHERE status = "left";
+
+
+    -- Add trigger to update changed_at field
+    CREATE TRIGGER IF NOT EXISTS update_users_changed_at
+           BEFORE UPDATE
+               ON users
+    BEGIN
+        UPDATE users
+           SET changed_at = CURRENT_TIMESTAMP
+         WHERE old.id = id;
+    END;
+
+    END;
+    '''
+    # conn = sqlite3.connect(DB_FILENAME)
+    conn = sqlite3.connect('./guild.db2')
+    cur = conn.cursor()
+    cur.executescript(migration)
+
+
+if __name__ == "__main__":
+    alter_users_table()
