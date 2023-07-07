b1380990ae838a3fd15ec739af4007e190391f93
Ruslan Kovtun <koutoftimer@gmail.com>
2023-07-08 01:05:33 +0300

    Refactor database, OCR, flow control
    * use sqlalchemy
    * convert image to grayscale for better recognition
    * clip screenshot relative to marked caption rathen then predefined
      coorinated
    * add couple of flags to control behavior

--- collect_donations.py
+++ collect_donations.py
@@ -1,14 +1,18 @@
 #!./venv/bin/python
+import argparse
 import json
 import logging
 import os
 import subprocess
 from pathlib import Path
 
-import requests
+import cv2
 import pyautogui
+import pytesseract
+import requests
+import sqlalchemy.orm as orm
 
-from db import save_donations_to_db
+from db import save_donations_to_db, engine, User
 
 DONATIONS_RECT = 448, 174, 535, 885
 
@@ -33,7 +37,10 @@ class GuildScreen:
         pyautogui.sleep(1)
 
 
-DONATION_SCREENSHOTS = [Path('1.png'), Path('2.png')]
+DONATION_SCREENSHOTS = [
+    Path('1.png'),
+    Path('2.png'),
+]
 
 
 def take_donations_screenshots():
@@ -49,24 +56,55 @@ def take_donations_screenshots():
     pyautogui.scroll(50)
     pyautogui.sleep(1)
     # top donators
-    pyautogui.screenshot(DONATION_SCREENSHOTS[0], region=DONATIONS_RECT)
+    pyautogui.screenshot(DONATION_SCREENSHOTS[0])
     pyautogui.sleep(5)
     # scroll bottom
     pyautogui.scroll(-50)
     pyautogui.sleep(1)
     # rest donators
-    pyautogui.screenshot(DONATION_SCREENSHOTS[1], region=DONATIONS_RECT)
+    pyautogui.screenshot(DONATION_SCREENSHOTS[1])
     pyautogui.sleep(5)
     # get back to main screen
     pyautogui.press('esc', presses=2, interval=1)
 
 
+def get_black_white(file_name):
+    img = cv2.imread(file_name)
+
+    cv2.imread(file_name)
+    img2gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(img2gray, 200, 255, cv2.THRESH_BINARY)
+    image_final = cv2.bitwise_and(img2gray, img2gray, mask=mask)
+    _, new_img = cv2.threshold(
+        image_final, 180, 255,
+        cv2.THRESH_BINARY)  # for black text , cv.THRESH_BINARY_INV
+
+    return new_img
+
+
 def ocr_screenshot(screenshot: str) -> dict[str, int]:
-    raw = subprocess.getoutput(fr'''
-        tesseract {screenshot} - \
-            --psm 6 --oem 0 --tessdata-dir ./tessdata/ \
-            -l eng+rus+chi_sim+chi_tra+jpn
-    ''')
+    fullscreen = cv2.imread(screenshot)
+    player = cv2.imread('samples/Player.png')
+    result = cv2.matchTemplate(player, fullscreen, cv2.TM_SQDIFF_NORMED)
+    _, _, (x, y), _ = cv2.minMaxLoc(result)
+    x += 5
+    y += 90
+
+    black_white = get_black_white(screenshot)[
+        y:y + DONATIONS_RECT[3],
+        x:x + DONATIONS_RECT[2],
+    ]
+
+    raw = pytesseract.image_to_string(
+        black_white,
+        lang='rus+eng',
+        config='--psm 6 --oem 0 --tessdata-dir ./tessdata/')
+    # raw = subprocess.getoutput(fr'''
+    #     tesseract {screenshot} - \
+    #         --psm 6 --oem 0 --tessdata-dir ./tessdata/ \
+    #         -l eng+rus
+    # ''')
+    # # -l eng+rus+chi_sim+chi_tra+jpn
     NICK_CORRECTIONS = {
         'eItooth': 'eltooth',
         'シ ヤ ガー': 'ジャガー',
@@ -78,7 +116,7 @@ def ocr_screenshot(screenshot: str) -> dict[str, int]:
             continue
         nick, donation = line.rsplit(maxsplit=1)
         nick = NICK_CORRECTIONS.get(nick, nick)
-        data[nick] = int(donation.replace(',', ''))
+        data[nick] = int(donation.replace(',', '').replace('о', '0'))
     return data
 
 
@@ -103,19 +141,48 @@ def push_updates_to_pythonanywhere(data: dict[str, int]):
 
 def main():
     logging.getLogger().setLevel(logging.DEBUG)
-    pyautogui.FAILSAFE = True
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--new', action='store_true')
+    parser.add_argument('--save', action='store_true')
+    parser.add_argument('--upload', action='store_true')
+    args = parser.parse_args()
 
     # TODO: rewrite with python ffi for libx11
-    subprocess.getoutput(
-        "xdotool search --name '^Firestone$' windowactivate --sync")
-    take_donations_screenshots()
+    if args.new:
+        pyautogui.FAILSAFE = True
+        subprocess.getoutput(
+            "xdotool search --name '^Firestone$' windowactivate --sync")
+        take_donations_screenshots()
 
     data = ocr(DONATION_SCREENSHOTS)
-    logging.debug(f'OCR {data}')
-    save_donations_to_db(data)
+    from pprint import pprint
+    pprint(data)
 
-    # replicate data to pythonanywhere
-    push_updates_to_pythonanywhere(data)
+    with orm.Session(engine) as session:
+        old = set(nick[0] for nick in session.query(User.nick).filter_by(
+            status='active').all())
+    new = set(data.keys())
+
+    if old != new:
+        print(f"Old: {old}")
+        print(
+            f'Leavers: {old - new},\n new: {new - old},\n remains: {len(old & new)}'
+        )
+        while (ans :=
+               input('Proced? (y/N): ')) not in ['y', 'Y', 'n', 'N', '']:
+            pass
+        if ans == '':
+            ans = 'n'
+        if ans.lower() == 'n':
+            return
+
+    if args.save:
+        save_donations_to_db(data)
+
+    if args.upload:
+        # replicate data to pythonanywhere
+        push_updates_to_pythonanywhere(data)
 
 
 if __name__ == "__main__":
--- db.py
+++ db.py
@@ -1,137 +1,71 @@
-import logging
-import sqlite3
-from typing import Iterable
+import datetime
 
-DB_FILENAME = './guild.db'
+import sqlalchemy as sa
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column, Session
+
+DB_FILENAME = 'guild.db'
+engine = sa.create_engine(f"sqlite:///{DB_FILENAME}", echo=True, future=True)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nick: Mapped[str] = mapped_column(unique=True)
+    joined_at: Mapped[datetime.datetime] = mapped_column(
+        default=datetime.datetime.utcnow)
+    status: Mapped[str]
+    comment: Mapped[str] = mapped_column(default='')
+
+    donations: Mapped[list['Donations']] = relationship('Donations',
+                                                        back_populates='user')
+
+    def __repr__(self) -> str:
+        return (f"<User: {self.id}> {self.nick} - {self.status}, "
+                "{self.joined_at}, {self.comment}")
+
+
+class Donations(Base):
+    __tablename__ = 'donations'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    amount: Mapped[int]
+    timestamp: Mapped[datetime.datetime] = mapped_column(
+        default=datetime.datetime.utcnow)
+    user_id = sa.Column(sa.Integer, sa.ForeignKey('users.id'), nullable=False)
+
+    user: Mapped[User] = relationship('User', back_populates='donations')
+
+
+Base.metadata.create_all(engine)
 
 
 def save_donations_to_db(data: dict[str, int]):
-    logging.info(f'Updating donations: {data}')
-    update_member_status(data.keys())
-    create_table = '''
-    CREATE TABLE IF NOT EXISTS donations (
-        user_id INTEGER NOT NULL,
-        donation INTEGER NOT NULL,
-        timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        id INTEGER PRIMARY KEY ASC,
-        FOREIGN KEY(user_id) REFERENCES user(id)
-    )
-    '''
-    conn = sqlite3.connect(DB_FILENAME)
-    cur = conn.cursor()
-    cur.execute(create_table)
-    cur.executemany(
-        '''
-        INSERT INTO donations (user_id, donation)
-        VALUES ((SELECT id FROM users WHERE nickname = ?), ?)
-        ''',
-        tuple(data.items()),
-    )
-    conn.commit()
+    with Session(engine) as session:
+        # mark active guild member
+        session.query(User).filter(User.nick.in_(data.keys())).update(
+            {User.status: 'active'})
+        # mark members that left the guild
+        session.query(User).filter(User.nick.not_in(data.keys())).update(
+            {User.status: 'left'})
+        # add new members
+        old = set(nick[0] for nick in session.query(User.nick).filter(
+            User.nick.in_(data.keys())).all())
+        new = set(data.keys())
+        session.add_all(User(nick=nick, status='active') for nick in new - old)
+        session.commit()
 
-
-def update_member_status(active_members: Iterable[str]):
-    from itertools import chain
-    create_table = '''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY ASC,
-        nickname TEXT UNIQUE NOT NULL,
-        status TEXT NOT NULL,
-        changed_at NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        created_at NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        comment TEXT
-    )
-    '''
-    conn = sqlite3.connect(DB_FILENAME)
-    cur = conn.cursor()
-    cur.execute(create_table)
-
-    cur.execute("SELECT nickname FROM users WHERE status = 'active'")
-    old_active = list(chain.from_iterable(cur.fetchall()))
-    print(old_active)
-
-    cur.executemany(
-        "UPDATE users SET status = 'left' WHERE nickname IN (?)",
-        zip(set(old_active) - set(active_members)),
-    )
-    cur.executemany(
-        '''
-        INSERT INTO users (nickname, status) VALUES (?, 'active') 
-        ON CONFLICT (nickname) DO UPDATE SET status = 'active'
-        ''',
-        zip(set(active_members) - set(old_active)),
-    )
-    conn.commit()
-
-
-# Migrations
-
-
-def alter_users_table():
-    migration = '''
-    -- Add changed_at and created_at columns to users table
-    BEGIN;
-
-    -- Create temporary table
-    CREATE TABLE IF NOT EXISTS users_new (
-        id INTEGER PRIMARY KEY ASC,
-        nickname TEXT UNIQUE NOT NULL,
-        status TEXT NOT NULL,
-        changed_at NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        created_at NOT NULL DEFAULT CURRENT_TIMESTAMP,
-        comment TEXT
-    );
-
-    -- Data migration
-    INSERT INTO users_new (id, nickname, status, comment)
-        SELECT * FROM users;
-
-    -- Replace temporary table with original
-    DROP TABLE users;
-    ALTER TABLE users_new RENAME TO users;
-
-    -- Set correct creation time (this is correct only now)
-    UPDATE users
-       SET created_at = (SELECT min(timestamp) FROM donations WHERE user_id = users.id),
-           changed_at = ifnull(
-               (SELECT min(timestamp)
-                 FROM donations
-                WHERE timestamp > (
-                    SELECT max(timestamp)
-                      FROM (
-                         SELECT timestamp, group_concat('<' || user_id || '>') as members
-                           FROM donations
-                         GROUP BY timestamp
-                      )
-                     WHERE members NOT LIKE '%<' || users.id || '>%'
-                )),
-                (SELECT min(timestamp) FROM donations WHERE user_id = users.id)
-           );
-
-    -- changed_at differes for those who left
-    UPDATE users
-       SET changed_at = (
-           SELECT max(timestamp) FROM donations WHERE user_id = users.id
-       )
-     WHERE status = "left";
-
-
-    -- Add trigger to update changed_at field
-    CREATE TRIGGER IF NOT EXISTS update_users_changed_at
-           BEFORE UPDATE
-               ON users
-    BEGIN
-        UPDATE users
-           SET changed_at = CURRENT_TIMESTAMP
-         WHERE old.id = id;
-    END;
-
-    END;
-    '''
-    conn = sqlite3.connect(DB_FILENAME)
-    cur = conn.cursor()
-    cur.executescript(migration)
-
-
-if __name__ == "__main__":
-    alter_users_table()
+        # add new donation records
+        nick_to_id = dict(
+            session.query(User.nick,
+                          User.id).filter(User.nick.in_(data.keys())))
+        session.add_all(
+            Donations(
+                user_id=nick_to_id[nick],
+                amount=amount,
+            ) for nick, amount in data.items())
+        session.commit()
